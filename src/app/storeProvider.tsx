94fb39030bba74c56b24c7910d54e4a09058f3bc
talpidaex <bayrakogzhn@gmail.com>
2024-06-09 17:23:42 +0300

    feat: Prettier installed

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -1,25 +1,20 @@
-'use client'
-import { AppStore, makeStore } from "@/lib/store"
-import { useRef } from "react"
-import { Provider } from "react-redux"
+"use client";
+import { AppStore, makeStore } from "@/lib/store";
+import { useRef } from "react";
+import { Provider } from "react-redux";
 
 interface Props {
-    readonly children: React.ReactNode
+  readonly children: React.ReactNode;
 }
 
 const StoreProvider = ({ children }: Props) => {
-    const storeRef = useRef<AppStore | null>(null)
+  const storeRef = useRef<AppStore | null>(null);
 
-    if (!storeRef.current) {
-        storeRef.current = makeStore()
-    }
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
 
-    return (
-        <Provider store={storeRef.current}>
-            {children}
-        </Provider>
-    )
+  return <Provider store={storeRef.current}>{children}</Provider>;
+};
 
-}
-
-export default StoreProvider
+export default StoreProvider;
